e12692bd50bafaafc369c716d997971709e4ef79
billaliq <67883529+Musab45@users.noreply.github.com>
2023-11-23 14:21:39 +0500

    Update Lab_MidTerm.cpp
    Prevent Negative Stock

--- Lab_MidTerm.cpp
+++ Lab_MidTerm.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	//Initialitaion
 	int total_stock, stock_min, remaining_stock, restock_amount, sale;
+
+	//Instructions
 	
 	//Get Data From User
 	cout << "Enter Total Quanitity of Stock: ";
@@ -17,28 +19,32 @@ int main()
 	//Loop to Keep Program Running
 	while (remaining_stock > 0)
 	{
-		//Get Sales Data
-		cout << "Enter Number of Items Sold: ";
-		cin >> sale;
-		remaining_stock = remaining_stock - sale;
-		cout << "\nRemaining Stock is " << remaining_stock << endl;
-		
-		//Restock Process
-		if (remaining_stock < stock_min)
-		{
-			while (remaining_stock < stock_min)
-			{
-				cout << "\nStock Below Minimum Quantitiy!" << endl;
-				cout << "Enter Restock Amount : ";
-				cin >> restock_amount;
-				remaining_stock += restock_amount;
-				cout << "\nRemaining Stock is " << remaining_stock << endl;
-			}
-		}
-		
-		//Show Remaining Stock
-		cout << "\nRemaining Stock is " << remaining_stock << endl;
-	}
+			//Get Sales Data
+			cout << "\nEnter Number of Items Sold: ";
+			cin >> sale;
+			remaining_stock = remaining_stock - sale;
+			cout << "\nRemaining Stock is " << remaining_stock << endl;
 
+			//Restock Process
+			if (remaining_stock < stock_min)
+			{
+				while (remaining_stock < stock_min)
+				{
+					if (remaining_stock >= 0)
+					{
+						cout << "\nStock Below Minimum Quantitiy!" << endl;
+						cout << "Enter Restock Amount : ";
+						cin >> restock_amount;
+						remaining_stock += restock_amount;
+						cout << "\nRemaining Stock is " << remaining_stock << endl;
+					}
+					else
+					{
+						cout << "\nWrong Input!";
+						break;
+					}
+				}
+			}
+	}
 	return 0;
 }
